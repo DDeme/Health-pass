b5bcdb1cf5756e4472ea0d62e24e11ba63cf7ccd
Daniel Demečko <daniel@demecko.com>
2020-04-22 15:16:28 +0200

    Updates

---
+++ precache-manifest.521d582382a5abdd698ceeff28202d56.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b75ecf9e6126081f421d164c114bb72",
+    "url": "/Health-pass/index.html"
+  },
+  {
+    "revision": "1184e819e1536bdec608",
+    "url": "/Health-pass/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "151dec3ea28a50efb3b6",
+    "url": "/Health-pass/static/css/main.f4e9e51b.chunk.css"
+  },
+  {
+    "revision": "1184e819e1536bdec608",
+    "url": "/Health-pass/static/js/2.48ef277a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Health-pass/static/js/2.48ef277a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151dec3ea28a50efb3b6",
+    "url": "/Health-pass/static/js/main.17aa82e7.chunk.js"
+  },
+  {
+    "revision": "fee44851a2e1ee43501a",
+    "url": "/Health-pass/static/js/runtime-main.120539de.js"
+  },
+  {
+    "revision": "42a5ae880afaa61368f2fa3d58327190",
+    "url": "/Health-pass/static/media/icons.42a5ae88.svg"
+  },
+  {
+    "revision": "6e6a6495ca5b063e754cb6ed90383849",
+    "url": "/Health-pass/static/media/icons.6e6a6495.ttf"
+  },
+  {
+    "revision": "6f6e7fce66619422ed3c46bc4d77ef56",
+    "url": "/Health-pass/static/media/icons.6f6e7fce.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.546b3545624b1d93e097d3bbb7c58860.js"
+  "/Health-pass/precache-manifest.521d582382a5abdd698ceeff28202d56.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Health-pass/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
