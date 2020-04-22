26db729bbae1e093af108500e508db015d1e9c2e
Daniel Demečko <daniel@demecko.com>
2020-04-22 15:12:58 +0200

    Updates

---
+++ precache-manifest.546b3545624b1d93e097d3bbb7c58860.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f77cbb1b7c0daa3cc86f2d87a7877d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbc59d328541c53d8e14",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "935d22f24822615f0aae",
+    "url": "/static/css/main.276a3b53.chunk.css"
+  },
+  {
+    "revision": "cbc59d328541c53d8e14",
+    "url": "/static/js/2.cadd9a73.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cadd9a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "935d22f24822615f0aae",
+    "url": "/static/js/main.8afbefe2.chunk.js"
+  },
+  {
+    "revision": "95aaa809cfec3e9f0b4c",
+    "url": "/static/js/runtime-main.90cf172d.js"
+  },
+  {
+    "revision": "42a5ae880afaa61368f2fa3d58327190",
+    "url": "/static/media/icons.42a5ae88.svg"
+  },
+  {
+    "revision": "6e6a6495ca5b063e754cb6ed90383849",
+    "url": "/static/media/icons.6e6a6495.ttf"
+  },
+  {
+    "revision": "6f6e7fce66619422ed3c46bc4d77ef56",
+    "url": "/static/media/icons.6f6e7fce.woff"
+  }
+]);
