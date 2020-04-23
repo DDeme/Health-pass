490a8988bb477df6325e73443eef569c944f0fce
Daniel Demečko <daniel@demecko.com>
2020-04-24 00:22:25 +0200

    scanning and verification library

--- front-end/src/Pages/Scanning/index.tsx
+++ front-end/src/Pages/Scanning/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Scanner } from '../../components/Scanner/index';
+// import { Scanner } from '../../components/Scanner/index';
+import { verifyPass } from '../../services/verifyPass';
 
 export const Scanning = () => {
-	return <Scanner />
-}
-	
+	 const res = JSON.stringify(verifyPass())
+	return <span>{res}</span>
+}	
