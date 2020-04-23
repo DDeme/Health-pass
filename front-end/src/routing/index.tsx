e63592af2d2eda5981fa4f3f99485e03b789dc71
Daniel Demečko <daniel@demecko.com>
2020-04-23 16:21:11 +0200

    fixing base path adn broken build

--- front-end/src/routing/index.tsx
+++ front-end/src/routing/index.tsx
@@ -2,12 +2,13 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Icons, AdminNavBar } from '../components'
-import { SplashScreen, Loading, Home, Login, Verification } from '../pages'
+import { SplashScreen, Loading, Home, Login, Verification } from '../Pages'
+import { PUBLIC_URL } from '../const'
 
 const Routing: any = (): any => {
 	return (
 		<React.Suspense fallback={<Loading />}>
-			<BrowserRouter>
+			<BrowserRouter basename={PUBLIC_URL}>
 				<AdminNavBar />
 				<Switch>
 					<Route exact path="/">
