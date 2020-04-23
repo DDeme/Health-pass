5d1c2359c2f6a15e9410082e0ebcb2a47543fe5d
Daniel Demečko <daniel@demecko.com>
2020-04-24 01:53:16 +0200

    fixed broken build

--- front-end/src/services/index.ts
+++ front-end/src/services/index.ts
@@ -18,7 +18,7 @@ interface VerifiedCertificate extends Certificate {
 const certificate =
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJhZ2UiOiIyOCIsInJlZ2lvbiI6IlBvcHJhZCIsImNvdW50cnkiOiJTbG92YWtpYSIsIm1vdmVtZW50QWxsb3dlZCI6dHJ1ZSwiaWF0IjoxNTg3Njc5NzA4LCJleHAiOjE1ODc3NjYxMDh9.oLzlPFFxzzbxtqV9gtnAJkAg6OYlPKSPxqOeBlWRNug'
 
-const verifyJWT = (token): boolean => true
+// const verifyJWT = (token): boolean => true
 
 const parseJWT = (token = certificate) => {
 	const data = jwt.decode(token)
