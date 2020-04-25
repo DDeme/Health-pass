87ad605980ee89f80cf37613a75440e3c1f04c82
ci-build <ci-build@ddeme.com>
2020-04-25 15:07:41 +0000

    Updates

---
+++ precache-manifest.319bf646411a4e1d940d1a7067eefa20.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ea8e9edcd273b9c295a6ba83d873d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b6f63c15eaa7e138ff5",
+    "url": "/static/css/main.276a3b53.chunk.css"
+  },
+  {
+    "revision": "1accfd8bff6d2febd439",
+    "url": "/static/js/2.71a1cc0c.chunk.js"
+  },
+  {
+    "revision": "4d3d17ca1d72509d468eda11f7e92a3e",
+    "url": "/static/js/2.71a1cc0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b6f63c15eaa7e138ff5",
+    "url": "/static/js/main.61189b78.chunk.js"
+  },
+  {
+    "revision": "95aaa809cfec3e9f0b4c",
+    "url": "/static/js/runtime-main.90cf172d.js"
+  },
+  {
+    "revision": "42a5ae880afaa61368f2fa3d58327190",
+    "url": "/static/media/icons.42a5ae88.svg"
+  },
+  {
+    "revision": "6e6a6495ca5b063e754cb6ed90383849",
+    "url": "/static/media/icons.6e6a6495.ttf"
+  },
+  {
+    "revision": "6f6e7fce66619422ed3c46bc4d77ef56",
+    "url": "/static/media/icons.6f6e7fce.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Health-pass/precache-manifest.3e0df320a4753a1b5ff95c763d94f9d7.js"
+  "/precache-manifest.319bf646411a4e1d940d1a7067eefa20.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Health-pass/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
