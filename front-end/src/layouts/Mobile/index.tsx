7e84e4d582e25c4135ff395e62e64830318bca07
frantallukas10 <frantal.lukas10@gmail.com>
2020-04-25 03:58:38 +0200

    feat: MyStatus component WIP

--- front-end/src/layouts/Mobile/index.tsx
+++ front-end/src/layouts/Mobile/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import {
-	Container,
-	ContainerEnumType,
-	ContainerEnumPosition,
-	Menu,
-} from '../../components'
+import { Container, ContainerEnumType, ContainerEnumPosition, Menu } from '../../components'
 
 const Mobile: any = props => (
 	<Container
 		className={props.className}
 		type={ContainerEnumType.COL}
-		y={ContainerEnumPosition.TOP}>
+		y={props.y || ContainerEnumPosition.TOP}
+		x={props.x || ContainerEnumPosition.LEFT}>
 		<Menu />
 		<Container type={ContainerEnumType.COL}>{props.children}</Container>
 	</Container>
--- front-end/src/pages/MyStatus/index.tsx
+++ front-end/src/pages/MyStatus/index.tsx
@@ -1,17 +1,37 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
-import { Button } from '../../components'
-import { Mobile } from '../../layouts'
 import { Link as LinkR } from 'react-router-dom'
+import { Button, StatusBar, Icon, ContainerEnumType, ContainerEnumPosition, Container } from '../../components'
+import { Mobile } from '../../layouts'
 import { useTranslation } from 'react-i18next'
 
-const List: FC = styled.div`
-	max-width: 280px;
-	width: 100%;
+const Content: any = styled(Container)`
+	justify-content: flex-start;
 `
+
 const Link = styled(LinkR)`
 	display: inline-block;
 	width: 100%;
+`
+
+const InfoStatusBar: any = styled(StatusBar)`
+	padding: 20px;
+	flex: initial;
+
+	&:after {
+		display: none;
+	}
+`
+
+const Label: FC = styled.label`
+	color: ${({ theme }) => theme.color.white};
+	font-size: 24px;
+`
+
+const Title: FC = styled.h3`
+	color: ${({ theme }) => theme.color.white};
+	font-size: 24px;
+	padding-left: 20px;
 `
 
 const Item: FC = styled(Button)`
@@ -21,13 +41,24 @@ const Item: FC = styled(Button)`
 const Home = () => {
 	const { t } = useTranslation()
 
+	const data = {
+		positive: true,
+	}
+
 	return (
 		<Mobile>
-			<List>
+			<Content type={ContainerEnumType.COL} y={ContainerEnumPosition.TOP}>
+				<InfoStatusBar state={data.positive}>
+					<Container x={ContainerEnumPosition.LEFT}>
+						<Icon name={data.positive ? 'notification' : 'check'} />
+						<Title>{t('mystatus.positive.title')}</Title>
+					</Container>
+				</InfoStatusBar>
+				<Label>{t('mystatus.positive.label')}</Label>
 				<Link to="/">
 					<Item>{t('mystatus.button_one')}</Item>
 				</Link>
-			</List>
+			</Content>
 		</Mobile>
 	)
 }
