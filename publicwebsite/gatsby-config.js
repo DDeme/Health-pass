136e21748a59b96d4585f093c57138c7e7cf748f
Daniel Demečko <daniel@demecko.com>
2020-04-29 04:21:04 +0200

    fix missing dep

--- publicwebsite/gatsby-config.js
+++ publicwebsite/gatsby-config.js
@@ -7,19 +7,19 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: config.manifestName,
-        short_name: config.manifestShortName,
-        start_url: config.pathPrefix || config.manifestStartUrl,
-        background_color: config.manifestBackgroundColor,
-        theme_color: config.manifestThemeColor,
-        display: config.manifestDisplay,
-        icon: config.manifestIcon, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: config.manifestName,
+    //     short_name: config.manifestShortName,
+    //     start_url: config.pathPrefix || config.manifestStartUrl,
+    //     background_color: config.manifestBackgroundColor,
+    //     theme_color: config.manifestThemeColor,
+    //     display: config.manifestDisplay,
+    //     icon: config.manifestIcon, // This path is relative to the root of the site.
+    //   },
+    // },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline',
+    //  'gatsby-plugin-offline',
   ],
 };
